3e21d28d9dd1f6e66f301fd041485b341d07e31d
Maria Cecília Araújo <araujomariacecilia77@gmail.com>
2025-02-18 22:29:02 -0300

    criação do projeto front

---
+++ src/menus/vertical.js
@@ -0,0 +1,14 @@
+export default [
+  { heading: 'Menus' },
+  {
+    name: 'Home',
+    icon: 'mdi-view-dashboard-outline',
+    to: { name: 'home' },
+  },
+  {
+    name: 'Blank Page',
+    icon: 'mdi-file-outline',
+    to: { name: 'blank' },
+  },
+  { name: 'Documentation', icon: 'mdi-text-box-outline', href: 'https://docs.icreatorstudio.com/', target: '_blank' },
+]
